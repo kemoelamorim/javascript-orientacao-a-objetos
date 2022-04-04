f1429afa602582f35b66128d73340ee996d99fdc
Kemoel Amorim Miranda <kemoel.amorim@gmail.com>
2022-04-04 18:14:49 -0300

    Instancias de uma classe Cliente

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const cliente1Nome = "Ricardo"
-const cliente1CPF = "111.111.111-11"
-const cliente1Agencia = 1001
-const cliente1Saldo = 0
+class Cliente{
+  nome;
+  cpf;
+  agencia;
+  saldo;
+  rg;
+}
+const cliente1 = new Cliente()
+const cliente2 = new Cliente()
 
-const cliente2Nome = "Alice"
-const cliente2CPF = "222.222.222-22"
-const cliente2Agencia = 1001
-const cliente2Saldo = 0
+cliente1.nome = "Ricardo"
+cliente1.cpf = "111.111.111-11"
+cliente1.agencia = 1001
+cliente1.saldo = 0
+cliente1.rg = 892302
+console.log(cliente1)
 
-console.log(cliente1Nome)
-console.log(cliente2Nome)
+
+cliente2.nome = "Alice"
+cliente2.cpf = "222.222.222-22"
+cliente2.agencia = 1001
+cliente2.saldo = 0
+cliente2.rg = 2323242
+
+console.log(cliente2)
