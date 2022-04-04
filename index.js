553e7ef811d7453887c86f7466c06b89f55a2b22
Kemoel Amorim Miranda <kemoel.amorim@gmail.com>
2022-04-04 18:08:31 -0300

    Problema de repetição de códigos com programação estrutural

---
+++ index.js
@@ -0,0 +1,12 @@
+const cliente1Nome = "Ricardo"
+const cliente1CPF = "111.111.111-11"
+const cliente1Agencia = 1001
+const cliente1Saldo = 0
+
+const cliente2Nome = "Alice"
+const cliente2CPF = "222.222.222-22"
+const cliente2Agencia = 1001
+const cliente2Saldo = 0
+
+console.log(cliente1Nome)
+console.log(cliente2Nome)
